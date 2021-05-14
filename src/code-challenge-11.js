e0a841a4ec5454853b8decfcbb3b3b5fc4a1ef53
cmmerritt <christiane.merritt@gmail.com>
2021-05-14 16:48:16 -0700

    three tests pass

--- src/code-challenge-11.js
+++ src/code-challenge-11.js
@@ -9,6 +9,32 @@ export function findMax(matrix) {
     let max = Math.max(...element);
     newArr.push(max);
   }
-  console.log(newArr);
   return Math.max(...newArr);
 };
+
+// export function totalSum(matrix) {
+/*   let newArr = [];
+  if (matrix.length === 0) {
+    return 0;
+  } else {
+    for (let element of matrix) {
+      let sum = element.reduce(function(a, b) {
+        newArr.push;
+      }, matrix);
+      newArr.push(sum);
+      return sum;
+    }
+  }
+}; */
+
+export function totalSum(matrix) {
+  let sum = 0;
+  for (let x = 0; x < matrix.length; x++) {
+    if (matrix[x] instanceof Array) {
+      sum += totalSum(matrix[x]);
+    } else {
+      sum += matrix[x];
+    }
+  }
+  return sum;
+}
--- src/code-challenge-11.test.js
+++ src/code-challenge-11.test.js
@@ -1,4 +1,4 @@
-import { returnTen, findMax } from './code-challenge-11';
+import { returnTen, findMax, totalSum } from './code-challenge-11';
 
 test('it should return the last 10 characters of a string as an array', () => {
   expect(returnTen('hello world')).toStrictEqual(['e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']);
@@ -7,4 +7,9 @@ test('it should return the last 10 characters of a string as an array', () => {
 
 test('return the largest number', () => {
   expect(findMax([[13, 24, 24, 2], [2, 5, 6], [2, 3]])).toStrictEqual(24);
+});
+
+test('return the sum of all numbers', () => {
+  expect(totalSum([[13, 24, 24, 2], [2, 5, 6], [2, 3]])).toStrictEqual(81);
+  expect(totalSum([])).toStrictEqual(0);
 });
